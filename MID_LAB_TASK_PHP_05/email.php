cae9d4075caa94ffa3aaad8f3e2a0c2809bb61a3
tanzimazahiraurthy <tanzimazahir02@gmail.com>
2021-02-19 00:00:23 +0600

    Added and  resolved git issues

---
+++ MID_LAB_TASK_PHP_05/email.php
@@ -0,0 +1,34 @@
+<?php
+
+	
+	
+	if(isset($_POST['submit'])){
+
+		$email 		= $_POST['myname'];
+		
+		if($email == "" ){
+			echo "null submission...";
+		}else{
+			echo $email."<br>";
+
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Email Input</title>
+</head>
+<body>
+	<form method="post" action="#">
+		<fieldset>
+			<legend>Email</legend>
+			email: <input type="text" name="myname" value=""> <br>
+			
+			<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+	
+</body>
+</html>
